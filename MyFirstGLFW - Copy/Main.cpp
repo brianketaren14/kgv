5a60256673747c5d9bc35f4fa76a830d0a8799e5
Alex Mario Simanjuntak <80665374+Archieved@users.noreply.github.com>
2022-04-12 17:16:57 +0700

    Update Main.cpp
    final main.cpp

--- MyFirstGLFW - Copy/Main.cpp
+++ MyFirstGLFW - Copy/Main.cpp
@@ -1,7 +1,3 @@
-// UTS_Octagonal.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#define GLEW_STATIC
 #define _USE_MATH_DEFINES
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -24,14 +20,14 @@ float a, b = 0.0f;
 
 unsigned int program;
 GLint color1Loc, color2Loc, color3Loc, color4Loc, color5Loc, color6Loc, color7Loc, color8Loc, scaleloc;
-float r = 0.5;
+float scale = 0.5;
 
+//untuk menentukan titik / vertex pada octagon dengan parameter panjang sisi luar octagon dan menampung target array
 void octagonVertices(float r, float vertices[9][2]) {
     for (int i = 0; i < 8; i++) {
-        // 1, 2, 3, ..., 8
-        // 0, 1,
-        vertices[i + 1][0] = r * cos(2 * M_PI * i / 8); // 0,1,2,3,4,5,6,7
-        vertices[i + 1][1] = r * sin(2 * M_PI * i / 8);
+        
+        vertices[i + 1][0] = r * cos(2 * M_PI * i / 8); //untuk titik x
+        vertices[i + 1][1] = r * sin(2 * M_PI * i / 8); //untuk titik y
     }
 
 }
@@ -53,17 +49,7 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 
         }
 
-        /*std::cout << color1[0] << " " << color1[1] << " " << color1[2] << " " << color1[3] << std::endl;
-        std::cout << color2[0] << " " << color2[1] << " " << color2[2] << " " << color2[3] << std::endl;
-        std::cout << color3[0] << " " << color3[1] << " " << color3[2] << " " << color3[3] << std::endl;
-        std::cout << color4[0] << " " << color4[1] << " " << color4[2] << " " << color4[3] << std::endl;
-        std::cout << color5[0] << " " << color5[1] << " " << color5[2] << " " << color5[3] << std::endl;
-        std::cout << color6[0] << " " << color6[1] << " " << color6[2] << " " << color6[3] << std::endl;
-        std::cout << color7[0] << " " << color7[1] << " " << color7[2] << " " << color7[3] << std::endl;
-        std::cout << color8[0] << " " << color8[1] << " " << color8[2] << " " << color8[3] << std::endl;
-        */
-
-        //mengirim nilai ke color loc
+        //mengirim nilai ke color location
         glUniform4f(color1Loc, color1[0], color1[1], color1[2], color1[3]);
         glUniform4f(color2Loc, color2[0], color2[1], color2[2], color2[3]);
         glUniform4f(color3Loc, color3[0], color3[1], color3[2], color3[3]);
@@ -74,35 +60,36 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
         glUniform4f(color8Loc, color8[0], color8[1], color8[2], color8[3]);
 
     }
+
     // scaling / merubah ukuran octagon
     else if (key == GLFW_KEY_UP && action == GLFW_PRESS)
     {
-        if (r < 1.8)
+        if (scale < 1.8)
         {
-            r += 0.02;
+            scale += 0.02;
         }
-        else if (r >= 2) {
-            r += 0;
+        else if (scale >= 2) {
+            scale += 0;
         }
-        //std::cout << r << endl;
-        glUniform1f(scaleloc, r);
+        
+        glUniform1f(scaleloc, scale);
     }
     else if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
     {
-        if (r > 0.2)
+        if (scale > 0.2)
         {
-            r -= 0.02;
+            scale -= 0.02;
         }
-        else if (r <= 0) {
-            r -= 0;
+        else if (scale <= 0) {
+            scale -= 0;
         }
-        //std::cout << r << endl;
-        glUniform1f(scaleloc, r);
+        
+        glUniform1f(scaleloc, scale);
     }
 
 }
 
-int main(void) //main program
+int main(int key, int scancode, int action, int mods) //main program
 {
     GLFWwindow* window;
 
@@ -111,7 +98,7 @@ int main(void) //main program
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(1280, 720, "Projek UTS KGV", NULL, NULL);
+    window = glfwCreateWindow(480, 480, "Projek UTS KGV", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -128,12 +115,13 @@ int main(void) //main program
 
     // vertex
     float vertices[9][2] = {
-        {0,0 }, // 0
+        {0,0 }
     };
 
+    //memanggil fungsi dibawah ini untuk mendeklarasikan titik-titik pada octagon
     octagonVertices(1, vertices);
 
-    // index untuk gambar 
+    // index untuk menggambar octagon
     unsigned int indexArr[] = {
         0,1,2,
         0,2,3,
@@ -145,20 +133,17 @@ int main(void) //main program
         0,8,1
     };
 
-    // uniform adalah sebuah variabel yang dikirimkan dari cpu ke gpu
-
-
-    // untuk vertex nya
+    // untuk mengirim vertexbuffer ke shader
     unsigned int vertexBuffer = 0;
     glGenBuffers(1, &vertexBuffer);
-    // buffer tempat penyimpanan sementara
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer); // untuk mengikat(bind) vertexBuffer ke gl_array_buffer
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 9 * 2, vertices, GL_STATIC_DRAW); // jumlah vertex yng di deklarasikan
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 9 * 2, vertices, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-    // untuk  index buffernya
+    // untuk  index buffer ke shader
     unsigned int indexBuffer = 0;
     glGenBuffers(1, &indexBuffer);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer); // untuk mengikat indexBUffer ke gl_element_array
@@ -183,10 +168,12 @@ int main(void) //main program
     glAttachShader(program, fragmentShader);
     glLinkProgram(program);
 
-    //memasukkan nilai dari random generate
+    
+ //mengirimkan data tiap variabel uniform ke gpu
+    
     glUseProgram(program);
     scaleloc = glGetUniformLocation(program, "scale");
-    glUniform1f(scaleloc, r);
+    glUniform1f(scaleloc, scale);
 
     color1Loc = glGetUniformLocation(program, "color1");
     glUniform4f(color1Loc, color1[0], color1[1], color1[2], color1[3]);
@@ -216,21 +203,31 @@ int main(void) //main program
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        //membuat animasi dengan mengganti warna secara random di setiap polygon untuk setiap detiknya
-        currentTime = glfwGetTime();
-
-        a = (float)sin(currentTime) * (RAND_MAX);
-        b = (float)cos(currentTime) * (RAND_MAX);
         
-        glUniform4f(color1Loc, color1[0] * a, color1[1] * b, color1[2], color1[3] * a);
-        glUniform4f(color2Loc, color2[0] * a, color2[1] * a, color2[2], color2[3] * b);
-        glUniform4f(color3Loc, color3[0] * a, color3[1] * b, color3[2], color3[3] * a);
-        glUniform4f(color4Loc, color4[0] * b, color4[1] * a, color4[2], color4[3] * b);
-        glUniform4f(color5Loc, color5[0] * b, color5[1] * b, color5[2], color5[3] * a);
-        glUniform4f(color6Loc, color6[0] * b, color6[1] * a, color6[2], color6[3] * b);
-        glUniform4f(color7Loc, color7[0] * a, color7[1] * b, color7[2], color7[3] * a);
-        glUniform4f(color8Loc, color8[0] * b, color8[1] * a, color8[2], color8[3] * b);
+        //membuat animasi dengan mengganti warna secara random di setiap polygon untuk setiap detiknya
+     
+            for (int i = 0; i < 4; i++)
+            {
+                color1[i] = ((float)(rand() * 1.0f)) / RAND_MAX;
+                color2[i] = ((float)(rand() * 1.0f)) / RAND_MAX;
+                color3[i] = ((float)(rand() * 1.0f)) / RAND_MAX;
+                color4[i] = ((float)(rand() * 1.0f)) / RAND_MAX;
+                color5[i] = ((float)(rand() * 1.0f)) / RAND_MAX;
+                color6[i] = ((float)(rand() * 1.0f)) / RAND_MAX;
+                color7[i] = ((float)(rand() * 1.0f)) / RAND_MAX;
+                color8[i] = ((float)(rand() * 1.0f)) / RAND_MAX;
+            }
 
+            glUniform4f(color1Loc, color1[0], color1[1], color1[2], color1[3]);
+            glUniform4f(color2Loc, color2[0], color2[1], color2[2], color2[3]);
+            glUniform4f(color3Loc, color3[0], color3[1], color3[2], color3[3]);
+            glUniform4f(color4Loc, color4[0], color4[1], color4[2], color4[3]);
+            glUniform4f(color5Loc, color5[0], color5[1], color5[2], color5[3]);
+            glUniform4f(color6Loc, color6[0], color6[1], color6[2], color6[3]);
+            glUniform4f(color7Loc, color7[0], color7[1], color7[2], color7[3]);
+            glUniform4f(color8Loc, color8[0], color8[1], color8[2], color8[3]);
+            glfwWaitEventsTimeout(1);
+        
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -243,6 +240,7 @@ int main(void) //main program
 
         /* Poll for and process events */
         glfwPollEvents();
+
     }
 
     glfwTerminate();
